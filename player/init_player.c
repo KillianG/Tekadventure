1ef4496e2ed38f08f45dbc16e1386a12e8aff6dc
KillianG <killian.gardahaut@epitech.eu>
2017-09-10 14:46:21 +0200

    ADD : memset

--- player/init_player.c
+++ player/init_player.c
@@ -5,7 +5,7 @@
 ** Login   <killian.gardahaut@epitech.eu>
 ** 
 ** Started on  Wed Aug 30 17:59:04 2017 Killian
-** Last update Thu Sep  7 10:19:34 2017 Killian
+** Last update Sun Sep 10 14:24:14 2017 Killian
 */
 
 #include "tekadv.h"
@@ -15,6 +15,7 @@ t_player	*init_player()
   t_player      *player;
 
   player = malloc(sizeof(t_player));
+  player = memset(player, 0, sizeof(t_player));
   player->sprite = init_sprite(PLAYER,
                                vector2f(SCREEN_WIDTH/2 - 25, SCREEN_HEIGHT/2 - 25),
                                vector2f(1, 1));
