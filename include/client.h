09d850897ad9a5ae1dd4936270e8719b33c009aa
Marc PEREZ <marc.perez@epitech.eu>
2017-09-13 19:28:05 +0200

    includes cleanup and warnings

--- include/client.h
+++ include/client.h
@@ -5,12 +5,13 @@
 ** Login   <marc.perez@epitech.eu>
 ** 
 ** Started on  Fri Aug 25 14:07:25 2017 Marc PEREZ
-** Last update Mon Sep 11 14:03:31 2017 Marc PEREZ
+** Last update Wed Sep 13 19:27:35 2017 Marc PEREZ
 */
 
 #ifndef CLIENT_H_
 # define CLIENT_H_
 
+# include <stdbool.h>
 # include "tekadv.h"
 
 /*
@@ -31,5 +32,7 @@ t_player	*receive_data(void);
 */
 
 int		send_data(t_player *data);
+
+bool		send_all(int socket, void *buffer, size_t length);
 
 #endif /* !CLIENT_H_ */
--- network/misc.c
+++ network/misc.c
@@ -5,22 +5,11 @@
 ** Login   <marc.perez@epitech.eu>
 ** 
 ** Started on  Fri Aug 25 14:08:20 2017 Marc PEREZ
-** Last update Wed Sep 13 19:25:01 2017 Marc PEREZ
+** Last update Wed Sep 13 19:26:30 2017 Marc PEREZ
 */
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <netdb.h>
-#include <signal.h>
-#include <sys/socket.h>
-#include <fcntl.h>
-#include <err.h>
-#include "client.h"
-#include "game.h"
-#include "tekadv.h"
+#include <stdbool.h>
 
 bool	send_all(int socket, void *buffer, size_t length)
 {
