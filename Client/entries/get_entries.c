ca4788602f106e88945977905133866041613dfc
KillianG <killian.gardahaut@epitech.eu>
2017-10-05 14:09:19 +0200

    Added ZQSD handle

--- Client/entries/get_entries.c
+++ Client/entries/get_entries.c
@@ -5,20 +5,20 @@
 ** Login   <killian.gardahaut@epitech.eu>
 ** 
 ** Started on  Wed Aug 30 18:08:13 2017 Killian
-** Last update Tue Oct  3 15:55:57 2017 Killian
+** Last update Thu Oct  5 14:09:01 2017 Killian
 */
 
 #include "tekadv.h"
 
 void	get_entries(t_displayer *displayer)
 {
-  if (sfKeyboard_isKeyPressed(sfKeyLeft))
+  if (sfKeyboard_isKeyPressed(sfKeyQ))
     move_left(displayer);
-  if (sfKeyboard_isKeyPressed(sfKeyRight))
+  if (sfKeyboard_isKeyPressed(sfKeyD))
     move_right(displayer);
-  if (sfKeyboard_isKeyPressed(sfKeyDown))
+  if (sfKeyboard_isKeyPressed(sfKeyS))
     move_down(displayer);
-  if (sfKeyboard_isKeyPressed(sfKeyUp))
+  if (sfKeyboard_isKeyPressed(sfKeyZ))
     move_up(displayer);
   if (sfKeyboard_isKeyPressed(sfKeyE))
     {
@@ -28,7 +28,7 @@ void	get_entries(t_displayer *displayer)
     }
   if (sfMouse_isButtonPressed(sfMouseLeft))
     shoot(displayer);
-  if (sfKeyboard_isKeyPressed(sfKeyD))
+  if (sfKeyboard_isKeyPressed(sfKeyA))
     drop_weapon(displayer);
   update(displayer);
 }
