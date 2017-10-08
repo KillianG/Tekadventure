a9a06acc7e12e584b6f2c48376f672348233c5f5
KillianG <killian.gardahaut@epitech.eu>
2017-10-08 19:03:16 +0200

    fix bug

--- Client/mandatory/move.c
+++ Client/mandatory/move.c
@@ -5,7 +5,7 @@
 ** Login   <killian.gardahaut@epitech.eu>
 ** 
 ** Started on  Mon Oct  2 12:04:48 2017 Killian
-** Last update Sun Oct  8 15:38:30 2017 Killian
+** Last update Sun Oct  8 19:02:33 2017 Killian
 */
 
 #include "tekadv.h"
@@ -19,4 +19,5 @@ void	move_player_mouse(t_displayer *displayer)
     displayer->player->pos = move_forward(displayer->player->pos,
     displayer->player->angle,
     SPEED);
+  update_map(displayer);
 }
